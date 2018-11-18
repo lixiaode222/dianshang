9d70eef9472bd9b3c773259d6d92b28a72b6c376
lixiaode222 <1803118517@qq.com>
2018-11-18 12:29:45 +0000

    用户界面 - 商品列表

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    //商品列表页面
+    public function index(){
+
+        $products = Product::query()->where('on_sale',true)->paginate(16);
+
+        return view('products.index',compact('products'));
+    }
+}
