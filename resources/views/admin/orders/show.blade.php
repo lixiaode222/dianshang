262b20aeb67aa863d9ff45cee48809d7a6a0b21c
lixiaode222 <1803118517@qq.com>
2018-11-20 09:58:59 +0000

    后台管理 - 同意退款 - 支付宝退款

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -49,6 +49,8 @@
             <!-- 订单发货开始 -->
             <!-- 如果订单未发货，展示发货表单 -->
             @if($order->ship_status === \App\Models\Order::SHIP_STATUS_PENDING)
+                <!-- 加上这个判断条件 -->
+                @if($order->refund_status !== \App\Models\Order::REFUND_STATUS_SUCCESS)
                 <tr>
                     <td colspan="4">
                         <form action="{{ route('admin.orders.ship', [$order->id]) }}" method="post" class="form-inline">
@@ -84,6 +86,8 @@
                     <td>物流单号：</td>
                     <td>{{ $order->ship_data['express_no'] }}</td>
                 </tr>
+                <!-- 在 上一个 if 的 else 前放上 endif -->
+                @endif
             @endif
             <!-- 订单发货结束 -->
             @if($order->refund_status !== \App\Models\Order::REFUND_STATUS_PENDING)
@@ -150,5 +154,41 @@
                 });
             });
         });
+
+
+        // 同意按钮的点击事件
+        $('#btn-refund-agree').click(function() {
+            swal({
+                title: '确认要将款项退还给用户？',
+                type: 'warning',
+                showCancelButton: true,
+                closeOnConfirm: false,
+                confirmButtonText: "确认",
+                cancelButtonText: "取消",
+            }, function(ret){
+                // 用户点击取消，不做任何操作
+                if (!ret) {
+                    return;
+                }
+                $.ajax({
+                    url: '{{ route('admin.orders.handle_refund', [$order->id]) }}',
+                    type: 'POST',
+                    data: JSON.stringify({
+                        agree: true, // 代表同意退款
+                        _token: LA.token,
+                    }),
+                    contentType: 'application/json',
+                    success: function (data) {
+                        swal({
+                            title: '操作成功',
+                            type: 'success'
+                        }, function() {
+                            location.reload();
+                        });
+                    }
+                });
+            });
+        });
+
     });
 </script>
